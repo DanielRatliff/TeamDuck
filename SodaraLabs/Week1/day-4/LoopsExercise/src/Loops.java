5c6b5d63f2a22676a2d0c9b2e206bd1f7ae4d874
Sodara Y <ysodara89@gmail.com>
2021-06-24 13:10:22 -0700

    fixed index

--- SodaraLabs/Week1/day-4/LoopsExercise/src/Loops.java
+++ SodaraLabs/Week1/day-4/LoopsExercise/src/Loops.java
@@ -5,9 +5,9 @@ public class Loops {
 		//Start
 		int[] arr = new int [50];		
 		int i = 0;		
-		while (i < 50 ) {
+		while (i < arr.length ) {
 			arr[i] = i+1;
-			//System.out.println(arr[i]);
+			System.out.println(arr[i]);
 			i++;
 		}
 		//End
@@ -19,17 +19,17 @@ public class Loops {
 			if (arr[i] % 2 !=0) {
 				arr[i]= arr[i]+5;
 			}
-			//System.out.println(arr[i]);
+			System.out.println(arr[i]);
 			i++;
 		}
-		while(i<arr.length -1);
+		while(i<arr.length);
 		//End
 				
 		//Use a for loop to print each element of the array. Unless that element is a multiple of four or six. 
 		//If it's a multiple of four print FOUR!!. If it's a multiple of six print SIX!!!. 
 		//If it's a multiple of both print FOUR!!SIX!!!.
 		//Start
-		for (i = 0; i < arr.length-1; i++ ) {			
+		for (i = 0; i < arr.length; i++ ) {			
 			if ((arr[i] % 4 == 0) && (arr[i] % 6 == 0) ) {
 				System.out.println("FOUR!!SIX!!!");
 			} else if(arr[i] % 4 == 0) {
